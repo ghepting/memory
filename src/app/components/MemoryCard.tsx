89c2c662d1962bf99873c1d9b41eeba54c90454a
Gary Hepting <ghepting@gmail.com>
2024-05-09 10:27:22 -0400

    Created the game of Memory in react

--- src/app/components/MemoryCard.tsx
+++ src/app/components/MemoryCard.tsx
@@ -24,7 +24,7 @@ export default function MemoryCard({ card, disabled, matchedCards, selectedCards
           <StarFilledIcon />
         </div>
         <div className={styles.cardBack}>
-          <Image src={card.imageUrl} alt={card.key} width="220" height="220" />
+          <Image src={card.imageUrl} alt={card.key} width="360" height="360" />
         </div>
       </div>
     </div>
